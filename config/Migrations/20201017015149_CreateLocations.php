ff8d37b4e13d1976e37af580b4ed953e69433c4f
Andreas <amarkauskas1@gmail.com>
2020-10-17 13:38:44 +1100

    Migration files

---
+++ config/Migrations/20201017015149_CreateLocations.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateLocations extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('locations');
+        $table->addColumn('breed_id', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('seen', 'timestamp', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('lat', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 10,
+            'scale' => 6,
+        ]);
+        $table->addColumn('long', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 11,
+            'scale' => 6,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
