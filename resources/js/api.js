a55696d7d753e475e3cae13309a5d0ee2f791893
Andreas <amarkauskas1@gmail.com>
2020-10-21 20:21:13 +1100

    Fix: We don't need this token

--- resources/js/api.js
+++ resources/js/api.js
@@ -3,7 +3,6 @@ export default axios.create({
   baseURL: 'https://petspotter.joakal.com/api',
   timeout: 5000,
   headers: {
-    'X-Auth-Token': 'f2b6637ddf355a476918940289c0be016a4fe99e3b69c83d',
     'Content-Type': 'application/json'
   }
 })
